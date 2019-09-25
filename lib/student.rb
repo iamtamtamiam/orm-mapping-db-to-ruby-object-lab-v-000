c38a6a8faa77751d3de4bc77b5d2600c7a67b239
Tam Vu <vutam_a@hotmail.com>
2019-09-25 00:45:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,6 +39,19 @@ class Student
     end.first
   end
   
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = "9th"
+    SQL
+    
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
+    
+  end 
+  
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
